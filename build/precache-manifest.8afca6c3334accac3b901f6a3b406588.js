02405b8839f30f6e812df33651aa9fea6a059be5
arinsoni <85981514+arinsoni@users.noreply.github.com>
2023-05-09 19:19:28 +0530

    Add files via upload

---
+++ build/precache-manifest.8afca6c3334accac3b901f6a3b406588.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "84ace4d557b84e6ae422",
+    "url": "/static/css/main.8882a8f8.chunk.css"
+  },
+  {
+    "revision": "84ace4d557b84e6ae422",
+    "url": "/static/js/main.84ace4d5.chunk.js"
+  },
+  {
+    "revision": "7fc5741dfb5d402e95fa",
+    "url": "/static/js/1.7fc5741d.chunk.js"
+  },
+  {
+    "revision": "8b26172e0886e3b8ab33",
+    "url": "/static/js/2.8b26172e.chunk.js"
+  },
+  {
+    "revision": "72cba3a7707ecab0fd5b",
+    "url": "/static/js/runtime~main.72cba3a7.js"
+  },
+  {
+    "revision": "392ee57b6dfda3c208876601059b3e60",
+    "url": "/index.html"
+  }
+];
